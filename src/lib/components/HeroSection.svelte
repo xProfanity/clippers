f6c61f749c4f772637feaefa407d8a7ed7be4caf
got-crypto <pecpalmplay@gmail.com>
2023-09-19 00:23:27 +0200

    added hero bg image

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Cuts } from "$lib/index.js";
+</script>
+
+<div class="h-full w-full relative">
+  <div class="absolute z-0 h-full w-full">
+    <img src={Cuts} alt="bg" class="h-full w-full object-cover" />
+  </div>
+  <p>Hero, baby!</p>
+</div>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,10 @@
-import HeroSection from "./HeroSection.svelte";
+import HeroSection from "./components/HeroSection.svelte";
 
-export {
-    HeroSection
-};
+import Barbershop from './images/barbershop.jpg';
+import Clipper from './images/clipper.png';
+import Clippers from './images/clippers.jpg';
+import Cuts from './images/cuts.jpg';
+import Eugene from './images/eugene.jpg';
+
+export { Barbershop, Clipper, Clippers, Cuts, Eugene, HeroSection };
+
